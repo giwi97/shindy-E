2a3c4372375e521b320f02c2149a07c68c6c1a91
giwi97 <giwanthawijekoon@gmail.com>
2020-09-17 00:21:03 +0530

    vuetify installed

--- shindy-front/gridsome.config.js
+++ shindy-front/gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Shindy-E',
+  siteDescription: 'Create and explore events',
   plugins: []
 }
