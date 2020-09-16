97130ef231127d40ac1303a5e8ff756973cab0e0
giwi97 <giwanthawijekoon@gmail.com>
2020-09-17 01:46:53 +0530

    searchbar designed

--- shindy-front/src/layouts/Default.vue
+++ shindy-front/src/layouts/Default.vue
@@ -2,6 +2,17 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title>Shindy-E</v-toolbar-title>
+
+      <v-text-field 
+        label="Search" 
+        class="ml-8"
+        style="max-width: 300px"
+        append-icon="mdi-magnify"
+        outlined
+        rounded
+        dense
+        hide-details />
+
       <v-spacer />
       <v-btn>Create Event</v-btn>
     </v-app-bar>
