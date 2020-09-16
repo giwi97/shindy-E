f45983ef3ef199d5292c678b2936cb038786d9b2
giwi97 <giwanthawijekoon@gmail.com>
2020-09-17 01:33:28 +0530

    app bar created

--- shindy-front/src/layouts/Default.vue
+++ shindy-front/src/layouts/Default.vue
@@ -1,17 +1,14 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <p>{{$static.metadata.siteDescription}}</p>
-    <slot/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>Shindy-E</v-toolbar-title>
+      <v-spacer />
+      <v-btn>Create Event</v-btn>
+    </v-app-bar>
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
 </template>
 
 <static-query>
@@ -22,31 +19,3 @@ query {
   }
 }
 </static-query>
-
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
